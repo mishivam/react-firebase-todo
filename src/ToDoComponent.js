3bc235ba724f34b595d146bbd3eaf7fa9827b812
Shivam Mishra <mishivam1999@gmail.com>
2020-08-31 20:02:23 +0530

    All done

--- src/ToDoComponent.js
+++ src/ToDoComponent.js
@@ -6,24 +6,98 @@ import {
   ListItemText,
   IconButton,
   Tooltip,
+  Modal,
+  Backdrop,
+  makeStyles,
+  ButtonGroup,
+  Input,
+  Fade,
+  Button,
 } from "@material-ui/core";
-import { HighlightOff } from "@material-ui/icons";
+import { Edit, HighlightOff } from "@material-ui/icons";
 import db from "./firebase";
 import "./App.css";
+import firebase from "firebase";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function ToDoComponent(props) {
+  const [open, setOpen] = React.useState(false);
+  const [input, setInput] = React.useState(props.todo.todo);
+  const classes = useStyles();
+
   const deleteTodos = async (event) => {
     await db.collection("Todos").doc(props.todo.id).delete();
   };
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const updateTodo = async () => {
+    await db.collection("Todos").doc(props.todo.id).set(
+      {
+        ToDo: input,
+      },
+      { merge: true }
+    );
+    setOpen(false);
+  };
+
   return (
-    <div>
+    <>
+      <Modal
+        open={open}
+        className={classes.modal}
+        onClose={(e) => setOpen(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 400,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2>Update Todo</h2>
+            <Input
+              value={input}
+              onChange={(event) => setInput(event.target.value)}
+            />
+            <ButtonGroup aria-label="btn-grp">
+              <Button variant="contained" onClick={(e) => setOpen(false)}>
+                Cancel
+              </Button>
+              <Button variant="contained" onClick={updateTodo} color="primary">
+                Update
+              </Button>
+            </ButtonGroup>
+          </div>
+        </Fade>
+      </Modal>
+
       <List className="todoComp">
         <ListItem>
           <ListItemText
             primary={"📃\u00A0" + props.todo.todo}
             secondary={"🕐\u00A0" + Date(props.todo.timestamp)}
           />
+          <Tooltip title="Edit this todo ?">
+            <IconButton aria-label="Edit" onClick={handleOpen}>
+              <Edit />
+            </IconButton>
+          </Tooltip>
           <Tooltip title="Remove Todo">
             <IconButton aria-label="delete" onClick={deleteTodos}>
               <HighlightOff />
@@ -31,7 +105,7 @@ function ToDoComponent(props) {
           </Tooltip>
         </ListItem>
       </List>
-    </div>
+    </>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
-
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB2WyzYS2Wm0mUM89MoWzI9GFKSRJ9ImsE",
-    authDomain: "react-todo-7bfbc.firebaseapp.com",
-    databaseURL: "https://react-todo-7bfbc.firebaseio.com",
-    projectId: "react-todo-7bfbc",
-    storageBucket: "react-todo-7bfbc.appspot.com",
-    messagingSenderId: "413629280977",
-    appId: "1:413629280977:web:7eea93b9eb9cd115424bf0"
+  // goto firebase setting->project setting->Firebase sdk snipet->choose config->Copy from apikey to appId and paste here
 });
 
 const db = firebaseApp.firestore();
